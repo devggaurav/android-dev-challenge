8c22a9568067eba818b381803a64e211b85d11f3
Gaurav <gaurav@bjshomedelivery.com>
2021-10-06 15:23:55 +0530

    first commit

---
+++ app/src/main/java/com/bjs/androiddevchallenge/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.bjs.androiddevchallenge
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
